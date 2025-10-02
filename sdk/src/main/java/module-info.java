0b01dade88184c1616c013d4b053aa82285d9d05
david <mail@nonswag.dev>
2025-10-02 23:09:33 +0200

    Export incidents package and update requires

--- sdk/src/main/java/module-info.java
+++ sdk/src/main/java/module-info.java
@@ -1,4 +1,10 @@
 module net.thenextlvl.incidents {
+    exports net.thenextlvl.incidents;
+    
     requires static org.jetbrains.annotations;
     requires static org.jspecify;
+    requires com.google.common;
+    requires java.net.http;
+
+    uses net.thenextlvl.incidents.Incidents;
 }
